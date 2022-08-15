ecb1e80e5880e73e8b24af26393ad673038e63c6
Sean McBride <spmcbride1201@gmail.com>
2022-08-15 18:04:10 -0400

    build: sledge prot

--- Makefile
+++ Makefile
@@ -1,3 +1,29 @@
+CFLAGS=-O3 -I. -Wall
+LDFLAGS=-flto
+
+WASMCC=${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot/
+WASMCPP=${WASI_SDK_PATH}/bin/clang++ --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot/
+WASMCFLAGS=${CFLAGS}
+
+# See https://lld.llvm.org/WebAssembly.html
+WASMLDFLAGS=${LDFLAGS} -Wl,--allow-undefined,-z,stack-size=32768,--threads=1
+
+# Clang 12 WebAssembly Options
+# See https://clang.llvm.org/docs/ClangCommandLineReference.html#webassembly
+# Disable WebAssembly Proposals aWsm does not support
+WASMCFLAGS+= -mno-atomics # https://github.com/WebAssembly/threads
+WASMCFLAGS+= -mno-bulk-memory # https://github.com/WebAssembly/bulk-memory-operations
+WASMCFLAGS+= -mno-exception-handling # https://github.com/WebAssembly/exception-handling
+WASMCFLAGS+= -mno-multivalue # https://github.com/WebAssembly/multi-value
+# Mutable globals still exist, but disables the ability to import mutable globals
+WASMCFLAGS+= -mno-mutable-globals # https://github.com/WebAssembly/mutable-global
+WASMCFLAGS+= -mno-nontrapping-fptoint # https://github.com/WebAssembly/nontrapping-float-to-int-conversions
+WASMCFLAGS+= -mno-reference-types # https://github.com/WebAssembly/reference-types
+WASMCFLAGS+= -mno-sign-ext # https://github.com/WebAssembly/sign-extension-ops
+WASMCFLAGS+= -mno-tail-call # https://github.com/WebAssembly/tail-call
+WASMCFLAGS+= -mno-simd128 # https://github.com/webassembly/simd
+
+
 build:
 	mkdir -p build
 
@@ -5,16 +31,16 @@ out:
 	mkdir -p out
 
 build/sod.o: build sod_release_118/sod.c
-	clang -c sod_release_118/sod.c -o $@
+	clang ${CFLAGS} -c sod_release_118/sod.c -o $@
 
 build/sod.wasm.o: build sod_release_118/sod.c
-	${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot/ -D_WASI_EMULATED_MMAN -o $@ -c sod_release_118/sod.c
+	${WASMCC} ${WASMCFLAGS} -D_WASI_EMULATED_MMAN -o $@ -c sod_release_118/sod.c
 
 out/depth_to_xyz: out src/depth_to_xyz.cpp build/sod.o
-	clang++ -fpermissive -o $@ src/depth_to_xyz.cpp build/sod.o -Isod_release_118
+	clang++ ${CFLAGS} -fpermissive -o $@ src/depth_to_xyz.cpp build/sod.o -Isod_release_118
 
 out/depth_to_xyz.wasm: out src/depth_to_xyz.cpp build/sod.wasm.o
-	${WASI_SDK_PATH}/bin/clang++ --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot/ -D_WASI_EMULATED_MMAN -o $@ src/depth_to_xyz.cpp build/sod.wasm.o -Isod_release_118 -lwasi-emulated-mman
+	${WASMCPP} ${WASMCFLAGS} -D_WASI_EMULATED_MMAN -o $@ src/depth_to_xyz.cpp build/sod.wasm.o -Isod_release_118 -lwasi-emulated-mman
 
 .PHONY: run
 run: out/depth_to_xyz
--- src/depth_to_xyz.cpp
+++ src/depth_to_xyz.cpp
@@ -5,103 +5,102 @@
 
 #include "sod_img_writer.h"
 
-void stbi_write_to_stdout(void *context, void *data, int size)
+void
+stbi_write_to_stdout(void *context, void *data, int size)
 {
-    write(STDOUT_FILENO, data, size);
+	write(STDOUT_FILENO, data, size);
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-    if (argc < 3)
-    {
-        std::cout << argv[0] << " <rgb_size_bytes> <depth_size_bytes>\n";
-        exit(EXIT_FAILURE);
-    }
+	// for (auto i{ 0 }; i < argc; i++) { std::cerr << i << ": " << argv[i] << "\n"; }
 
-    // camera intrinsic param
-    float fx = 931.44895319;
-    float fy = 931.76842975;
-    float cx = 965.9112793;
-    float cy = 548.42700032;
+	if (argc < 3) {
+		std::cout << argv[0] << " <rgb_size_bytes> <depth_size_bytes>\n";
+		exit(EXIT_FAILURE);
+	}
 
-    /*
-    The following code is task of the server function
-    */
+	// camera intrinsic param
+	float fx = 931.44895319;
+	float fy = 931.76842975;
+	float cx = 965.9112793;
+	float cy = 548.42700032;
 
-    // rgb_xyz_image stores the output image, two 1080p image horizontally stitched side by side
-    // image resolution is (1920*2) * 1080
+	/*
+	The following code is task of the server function
+	*/
 
-    long rgb_buf_cap = strtol(argv[1], NULL, 10);
-    long depth_buf_cap = strtol(argv[2], NULL, 10);
+	// rgb_xyz_image stores the output image, two 1080p image horizontally stitched side by side
+	// image resolution is (1920*2) * 1080
 
-    // Read stdin into in-memory buffers for color and depth
-    char *rgb_buf = (char *)calloc(rgb_buf_cap + 1, sizeof(char));
-    char *depth_buf = (char *)calloc(depth_buf_cap + 1, sizeof(char));
+	long rgb_buf_cap   = strtol(argv[1], NULL, 10);
+	long depth_buf_cap = strtol(argv[2], NULL, 10);
 
-    std::cin.read(rgb_buf, rgb_buf_cap);
-    std::cin.read(depth_buf, depth_buf_cap);
+	// Read stdin into in-memory buffers for color and depth
+	char *rgb_buf   = (char *)calloc(rgb_buf_cap + 1, sizeof(char));
+	char *depth_buf = (char *)calloc(depth_buf_cap + 1, sizeof(char));
 
-    sod_img color = sod_img_load_from_mem((const unsigned char *)rgb_buf, rgb_buf_cap, 3);
-    sod_img depth = sod_img_load_from_mem((const unsigned char *)depth_buf, depth_buf_cap, 3);
+	std::cin.read(rgb_buf, rgb_buf_cap);
+	std::cin.read(depth_buf, depth_buf_cap);
 
-    // std::cerr << "color " << color.w << "x" << color.h << "x" << color.c << std::endl;
-    // std::cerr << "depth " << depth.w << "x" << depth.h << "x" << depth.c << std::endl;
+	sod_img color = sod_img_load_from_mem((const unsigned char *)rgb_buf, rgb_buf_cap, 3);
+	sod_img depth = sod_img_load_from_mem((const unsigned char *)depth_buf, depth_buf_cap, 3);
 
-    sod_img rgb_xyz_image = sod_make_image(2 * depth.w, depth.h, 3);
+	// std::cerr << "color " << color.w << "x" << color.h << "x" << color.c << std::endl;
+	// std::cerr << "depth " << depth.w << "x" << depth.h << "x" << depth.c << std::endl;
 
-    // std::cerr << "rgb_xyz_image " << rgb_xyz_image.w << "x" << rgb_xyz_image.h << "x" << rgb_xyz_image.c << std::endl;
+	sod_img rgb_xyz_image = sod_make_image(2 * depth.w, depth.h, 3);
 
-    // loop through every pixel location to process entire image
-    for (int v = 0; v < color.h; v++)
-    {
-        // copy 32 bit color image to left side of the output image
-        for (int u = 0; u < color.w; u++)
-        {
-            float r = sod_img_get_pixel(color, u, v, 0);
-            float g = sod_img_get_pixel(color, u, v, 1);
-            float b = sod_img_get_pixel(color, u, v, 2);
+	// std::cerr << "rgb_xyz_image " << rgb_xyz_image.w << "x" << rgb_xyz_image.h << "x" << rgb_xyz_image.c <<
+	// std::endl;
 
-            sod_img_set_pixel(rgb_xyz_image, u, v, 0, r);
-            sod_img_set_pixel(rgb_xyz_image, u, v, 1, g);
-            sod_img_set_pixel(rgb_xyz_image, u, v, 2, b);
-        }
+	// loop through every pixel location to process entire image
+	for (int v = 0; v < color.h; v++) {
+		// copy 32 bit color image to left side of the output image
+		for (int u = 0; u < color.w; u++) {
+			float r = sod_img_get_pixel(color, u, v, 0);
+			float g = sod_img_get_pixel(color, u, v, 1);
+			float b = sod_img_get_pixel(color, u, v, 2);
 
-        for (int u = 0; u < color.w; u++)
-        {
-            // float z_correct = static_cast<float>(depth_cv.at<uint16_t>(v, u));
-            // float x = z * (u - cx) / fx;
-            // float y = z * (v - cy) / fy;
+			sod_img_set_pixel(rgb_xyz_image, u, v, 0, r);
+			sod_img_set_pixel(rgb_xyz_image, u, v, 1, g);
+			sod_img_set_pixel(rgb_xyz_image, u, v, 2, b);
+		}
 
-            // rgb_xyz_image.at<cv::Vec3f>(v, u + depth.cols)[0] = x;
-            // rgb_xyz_image.at<cv::Vec3f>(v, u + depth.cols)[1] = y;
-            // rgb_xyz_image.at<cv::Vec3f>(v, u + depth.cols)[2] = z;
+		for (int u = 0; u < color.w; u++) {
+			// float z_correct = static_cast<float>(depth_cv.at<uint16_t>(v, u));
+			// float x = z * (u - cx) / fx;
+			// float y = z * (v - cy) / fy;
 
-            float z = sod_img_get_pixel(depth, u, v, 0);
-            // float z = depth.data[0*depth.h*depth.w + v * depth.w + u];
-            float x = z * (u - cx) / fx;
-            float y = z * (v - cy) / fy;
+			// rgb_xyz_image.at<cv::Vec3f>(v, u + depth.cols)[0] = x;
+			// rgb_xyz_image.at<cv::Vec3f>(v, u + depth.cols)[1] = y;
+			// rgb_xyz_image.at<cv::Vec3f>(v, u + depth.cols)[2] = z;
 
-            // std::cout << "z " << z << std::endl;
-            // std::cout << "z correct " << z_correct << std::endl << std::endl;
+			float z = sod_img_get_pixel(depth, u, v, 0);
+			// float z = depth.data[0*depth.h*depth.w + v * depth.w + u];
+			float x = z * (u - cx) / fx;
+			float y = z * (v - cy) / fy;
 
-            sod_img_set_pixel(rgb_xyz_image, u + depth.w, v, 0, x);
-            sod_img_set_pixel(rgb_xyz_image, u + depth.w, v, 1, y);
-            sod_img_set_pixel(rgb_xyz_image, u + depth.w, v, 2, z);
-        }
-    }
+			// std::cout << "z " << z << std::endl;
+			// std::cout << "z correct " << z_correct << std::endl << std::endl;
 
-    unsigned char *zPng = sod_image_to_blob(rgb_xyz_image);
-    if (zPng == nullptr)
-    {
-        return SOD_OUTOFMEM;
-    }
-    
-    int rc = stbi_write_png_to_func(stbi_write_to_stdout, NULL, rgb_xyz_image.w, rgb_xyz_image.h, rgb_xyz_image.c, (const void *)zPng, rgb_xyz_image.w * rgb_xyz_image.c);
-    sod_image_free_blob(zPng);
+			sod_img_set_pixel(rgb_xyz_image, u + depth.w, v, 0, x);
+			sod_img_set_pixel(rgb_xyz_image, u + depth.w, v, 1, y);
+			sod_img_set_pixel(rgb_xyz_image, u + depth.w, v, 2, z);
+		}
+	}
 
-    sod_free_image(color);
-    sod_free_image(depth);
-    sod_free_image(rgb_xyz_image);
+	unsigned char *zPng = sod_image_to_blob(rgb_xyz_image);
+	if (zPng == nullptr) { return SOD_OUTOFMEM; }
 
-    return rc ? SOD_OK : SOD_IOERR;
+	int rc = stbi_write_png_to_func(stbi_write_to_stdout, NULL, rgb_xyz_image.w, rgb_xyz_image.h, rgb_xyz_image.c,
+	                                (const void *)zPng, rgb_xyz_image.w * rgb_xyz_image.c);
+	sod_image_free_blob(zPng);
+
+	sod_free_image(color);
+	sod_free_image(depth);
+	sod_free_image(rgb_xyz_image);
+
+	return rc ? SOD_OK : SOD_IOERR;
 }
